6dc7322e36a82b4a6ac068f41ff61d81e808b5c8
Azamudin <muhamadazamudindollah@Muhamads-MacBook-Air.local>
2026-03-05 01:33:57 +0800

    feat(Q4): register new cubits and add flutter_map dependency
    Register SearchCubit (lazySingleton), OrderHistoryCubit (lazySingleton),
    SearchFood and GetOrderHistory use cases. Add flutter_map and latlong2
    packages for live map on order tracking.

--- Q4/source/food_delivery_app/lib/di/injection.dart
+++ Q4/source/food_delivery_app/lib/di/injection.dart
@@ -4,11 +4,15 @@ import '../features/food_delivery/data/datasources/food_delivery_local_datasourc
 import '../features/food_delivery/data/repositories/food_delivery_repository_impl.dart';
 import '../features/food_delivery/domain/repositories/food_delivery_repository.dart';
 import '../features/food_delivery/domain/usecases/get_menu_items.dart';
+import '../features/food_delivery/domain/usecases/get_order_history.dart';
 import '../features/food_delivery/domain/usecases/get_restaurants.dart';
+import '../features/food_delivery/domain/usecases/search_food.dart';
 import '../features/food_delivery/presentation/cubit/cart_cubit.dart';
 import '../features/food_delivery/presentation/cubit/menu_cubit.dart';
 import '../features/food_delivery/presentation/cubit/order_cubit.dart';
+import '../features/food_delivery/presentation/cubit/order_history_cubit.dart';
 import '../features/food_delivery/presentation/cubit/restaurant_list_cubit.dart';
+import '../features/food_delivery/presentation/cubit/search_cubit.dart';
 
 final getIt = GetIt.instance;
 
@@ -26,10 +30,18 @@ void setupDependencies() {
   // Use cases
   getIt.registerFactory(() => GetRestaurants(getIt<FoodDeliveryRepository>()));
   getIt.registerFactory(() => GetMenuItems(getIt<FoodDeliveryRepository>()));
+  getIt.registerFactory(() => SearchFood(getIt<FoodDeliveryRepository>()));
+  getIt
+      .registerFactory(() => GetOrderHistory(getIt<FoodDeliveryRepository>()));
 
   // Cubits
   getIt.registerFactory(() => RestaurantListCubit(getIt<GetRestaurants>()));
   getIt.registerFactory(() => MenuCubit(getIt<GetMenuItems>()));
+  getIt.registerLazySingleton(
+    () => SearchCubit(getIt<SearchFood>(), getIt<GetRestaurants>()),
+  );
   getIt.registerLazySingleton(() => CartCubit());
   getIt.registerFactory(() => OrderCubit());
+  getIt.registerLazySingleton(
+      () => OrderHistoryCubit(getIt<GetOrderHistory>()));
 }
